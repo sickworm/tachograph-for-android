076fe7992bff83e61ec09fa10fbba823d665a710
chenhao <ch.operation@gmail.com>
2014-05-06 14:52:20 +0800

    [Feature]complete the settings function

--- src/com/scutchenhao/tachograph/MainActivity.java
+++ src/com/scutchenhao/tachograph/MainActivity.java
@@ -2,6 +2,7 @@ package com.scutchenhao.tachograph;
 
 import android.os.Bundle;
 import android.os.IBinder;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ComponentName;
@@ -22,11 +23,11 @@ import com.scutchenhao.tachograph.MainService.LocalBinder;
 
 import android.graphics.PixelFormat;
 import android.hardware.Camera;
-import android.hardware.Camera.Size;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
 import android.media.MediaRecorder.OnInfoListener;
 import android.view.GestureDetector;
@@ -53,7 +54,7 @@ import android.util.Log;
 public class MainActivity extends Activity implements SurfaceHolder.Callback {
 	public final static int TURN_LEFT = 1;
 	public final static int TURN_RIGHT = 2;
-	public final static int DRAG_SENSITIVITY = 2500;
+	public final static int DRAG_SENSITIVITY = 3500;
 	public final static String DEFAULT_ID = "car1";
 	protected final static int TAKE_PICTURE_AMOUNT = 3;
 	protected final static int TAKE_PICTURE_DELAY = 1000;
@@ -68,22 +69,25 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
     private TextView log;
     private ScrollView scroll;
     private boolean isExpanding  = false;
+	private long backTime = 0;
     private MediaRecorder mMediaRecorder;
     private SurfaceView mSurfaceView;
     private boolean isRecording = false;
     private Camera mCamera;
     private StorageManager mStorageManager;
-	private int storageSettingPos = 0;
-	private int qualitySettingPos = 0;
-	private int timeSettingPos = 0;
-	private long backTime = 0;
+	private int storageSettingPos;
+	private int profileSettingPos;
+	private int timeSettingPos;
+	private int qualitySettingPos;
 	private int storageSetting;		//MB
 	private int remainStorage;		//MB
 	private int timeSetting;		//s
-	private int widthSetting = 320;
-	private int heightSetting = 240;
+	private int qualitySetting;		//%
+	private int widthSetting;
+	private int heightSetting;
 	private String idSetting = "";
-	private List<Size> supportedVideoSizes;
+	private List<CamcorderProfile> profileList = new ArrayList<CamcorderProfile>();
+	private List <String> profileNameList = new ArrayList<String>();
 	private SensorManager mSensorManager;
     private boolean takedPicture = true;
     private int takedPictureAmount = 0;
@@ -177,7 +181,30 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         mGestureDetector.setIsLongpressEnabled(true);
 
         mMediaRecorder = new MediaRecorder();
-
+        mMediaRecorder.setOnInfoListener(new OnInfoListener() {
+        	@Override
+        	public void onInfo(MediaRecorder mr, int what, int extra) {
+        		switch (what) {
+        		case MediaRecorder.MEDIA_RECORDER_INFO_UNKNOWN:
+                    log("录像出错，暂停录制", LOG_TOAST);
+            		stopRecording();
+            		setRecordState(false);
+        			break;
+        		case MediaRecorder.MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED:
+                    log("录像大小到，重新录像");
+        			break;
+        		case MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED:
+                    log("录像时间到，重新录像");
+        			restartRecording();
+        			break;
+        		default:
+        			break;
+        		}
+        	}
+        });
+        getQualityProfile();
+    	loadSettings();
+        
         mStorageManager = new StorageManager(this, storageSetting, remainStorage);
         int ret = mStorageManager.check();
         switch(ret) {
@@ -215,8 +242,6 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 
         mCamera = Camera.open();
 		Camera.Parameters parameters = mCamera.getParameters();
-		supportedVideoSizes = parameters.getSupportedVideoSizes();
-    	loadSettings();
     	parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_VIDEO);
     	parameters.setPreviewSize(widthSetting, heightSetting);
     	mCamera.setParameters(parameters);
@@ -242,9 +267,11 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 			stopRecording();
 		
 		mCamera.stopPreview();
+		mCamera.unlock();
 		mCamera.release();
 		mCamera = null;
 		mMediaRecorder.release();
+		mMediaRecorder = null;
 		
 		unbindService(mConnection);
 		mSensorManager.unregisterListener(mSensorEventListener);
@@ -306,48 +333,23 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 	}
 	
 	private boolean mediaRecorderConfig() {
-        mCamera.unlock();
+		mCamera.unlock();
         mMediaRecorder.setCamera(mCamera);
         mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        CamcorderProfile profile = profileList.get(profileSettingPos);
+        int bitRate = profile.videoBitRate * qualitySetting / 100;
+        mMediaRecorder.setProfile(profile);
+		mMediaRecorder.setVideoEncodingBitRate(bitRate);
         mMediaRecorder.setMaxDuration(timeSetting * 1000);
-        mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT);
-        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
-        // 设置视频录制的分辨率。必须放在设置编码和格式的后面，否则报错
-        mMediaRecorder.setVideoSize(widthSetting, heightSetting);
-        log("录像分辨率：" + widthSetting + "x" + heightSetting, LOG_SHOW_TEXT);
-        log("录像时间：" + (timeSetting/60) + "m", LOG_SHOW_TEXT);
-
+        
+        //文件体积=时间X码率/8
         if (!mStorageManager.checkNewRecordFile()) {
             log("录像文件错误", LOG_TOAST);
             return false;
         } else {
             mMediaRecorder.setOutputFile(mStorageManager.getFileName());
         }
-
-        mMediaRecorder.setOnInfoListener(new OnInfoListener() {
-        	@Override
-        	public void onInfo(MediaRecorder mr, int what, int extra) {
-        		switch (what) {
-        		case MediaRecorder.MEDIA_RECORDER_INFO_UNKNOWN:
-                    log("录像出错，暂停录制", LOG_TOAST);
-            		stopRecording();
-            		setRecordState(false);
-        			break;
-        		case MediaRecorder.MEDIA_RECORDER_INFO_MAX_FILESIZE_REACHED:
-                    log("录像大小到，重新录像");
-        			break;
-        		case MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED:
-                    log("录像时间到，重新录像");
-        			restartRecording();
-        			break;
-        		default:
-        			break;
-        		}
-        	}
-        });
-
         return true;
 	}
 	
@@ -396,6 +398,12 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         	setRecordState(false);
             start.setEnabled(false);
         }
+        
+        CamcorderProfile profile = profileList.get(profileSettingPos);
+        int bitRate = profile.videoBitRate * (qualitySetting / 100);
+        log("录像分辨率：" + widthSetting + "x" + heightSetting, LOG_SHOW_TEXT);
+        log("录像时间：" + (timeSetting/60) + "m", LOG_SHOW_TEXT);
+        log("录像比特率：" + bitRate, LOG_SHOW_TEXT);
 	}
 	
 	private void stopRecording() {		
@@ -414,7 +422,6 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
         try {
 	        if (mMediaRecorder != null) {
 	            mMediaRecorder.stop();
-	            mMediaRecorder.reset();
 	            int ret = mStorageManager.refreshDir(true);
 	            switch(ret) {
 	            case StorageManager.STORAGE_AVAILABLE:
@@ -479,6 +486,136 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 		scroll.setLayoutParams(params);
 	}
 	
+	private void loadSettings() {
+		SharedPreferences settings = getSharedPreferences("setting", MODE_PRIVATE);
+		storageSettingPos = settings.getInt("storage_pos", 0);
+		profileSettingPos = settings.getInt("profile_pos", 0);
+		timeSettingPos = settings.getInt("time_pos", 0);
+		qualitySettingPos = settings.getInt("quality_pos", 0);
+		storageSetting = settings.getInt("storage", getResources().getIntArray(R.array.storage)[0]);
+		timeSetting = settings.getInt("time", getResources().getIntArray(R.array.time)[0]);
+		qualitySetting = settings.getInt("quality", getResources().getIntArray(R.array.quality)[0]);
+		CamcorderProfile profile = profileList.get(profileSettingPos);
+		widthSetting = settings.getInt("width", profile.videoFrameWidth);
+		heightSetting = settings.getInt("height", profile.videoFrameHeight);
+		idSetting = settings.getString("id", DEFAULT_ID);
+	}
+	
+	private void settings() {
+        final View preferenceView = LayoutInflater.from(this).inflate(
+                R.layout.settings_dialog, null);
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(preferenceView);
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				SharedPreferences settings = getSharedPreferences("setting", MODE_PRIVATE);
+		        Editor mEditor = settings.edit();
+		
+		        Spinner recordProfile = (Spinner) preferenceView.findViewById(R.id.profile_select);
+		        int pos = recordProfile.getSelectedItemPosition();
+		        CamcorderProfile profile = profileList.get(pos);
+		        mEditor.putInt("profile_pos", pos);
+		        mEditor.putInt("width", profile.videoFrameWidth);
+		        mEditor.putInt("height", profile.videoFrameHeight);
+
+		        Spinner recordQuality = (Spinner) preferenceView.findViewById(R.id.quality_select);
+		        pos = recordQuality.getSelectedItemPosition();
+		        mEditor.putInt("quality_pos", pos);
+		        int[] quality = getResources().getIntArray(R.array.quality);
+		        mEditor.putInt("quality", quality[pos]);
+		        
+		        Spinner recordStorage = (Spinner) preferenceView.findViewById(R.id.storage_select);
+		        pos = recordStorage.getSelectedItemPosition();
+		        mEditor.putInt("storage_pos", pos);
+		        int[] storage = getResources().getIntArray(R.array.storage);
+		        mEditor.putInt("storage", storage[pos]);
+
+		        Spinner recordTime = (Spinner) preferenceView.findViewById(R.id.time_select);
+		        pos = recordTime.getSelectedItemPosition();
+		        mEditor.putInt("time_pos", pos);
+		        int[] time = getResources().getIntArray(R.array.time);
+		        mEditor.putInt("time", time[pos]);
+		
+		        EditText id = (EditText) preferenceView.findViewById(R.id.id);
+		        String idName = id.getText().toString();
+		        mEditor.putString("id", idName);
+		        
+		        mEditor.commit();
+		        mStorageManager.resetStorage(storage[pos]);
+		
+		        loadSettings();
+				mCamera.stopPreview();
+				Camera.Parameters parameters = mCamera.getParameters();
+		    	parameters.setPreviewSize(widthSetting, heightSetting);
+		    	mCamera.setParameters(parameters);
+		    	mCamera.startPreview();
+			}
+        });
+        loadDialogPreferences(preferenceView);
+        builder.show();
+	}
+	
+	private void loadDialogPreferences(View preferenceView) {
+        loadSettings();
+        Spinner recordProfile = (Spinner) preferenceView.findViewById(R.id.profile_select);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, profileNameList);
+        recordProfile.setAdapter(adapter);
+        recordProfile.setSelection(profileSettingPos);
+
+        Spinner recordStorage = (Spinner) preferenceView.findViewById(R.id.storage_select);
+        recordStorage.setSelection(storageSettingPos);
+
+        Spinner recordTime = (Spinner) preferenceView.findViewById(R.id.time_select);
+        recordTime.setSelection(timeSettingPos);
+
+        Spinner recordQuality = (Spinner) preferenceView.findViewById(R.id.quality_select);
+        recordQuality.setSelection(qualitySettingPos);
+        
+        EditText id = (EditText) preferenceView.findViewById(R.id.id);
+        id.setText(idSetting);
+	}
+	
+	@SuppressLint("InlinedApi")
+	private void getQualityProfile() {
+		if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_1080P)) {
+			profileList.add(CamcorderProfile.get(CamcorderProfile.QUALITY_1080P));
+			profileNameList.add("1080P(1920*1080)");
+		}
+		if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_720P)) {
+			profileList.add(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
+			profileNameList.add("720P(180*720)");
+		}
+		if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_480P)) {
+			profileList.add(CamcorderProfile.get(CamcorderProfile.QUALITY_480P));
+			profileNameList.add("480P(720*480)");
+		}
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+			if (CamcorderProfile.hasProfile(CamcorderProfile.QUALITY_QVGA)) {
+				profileList.add(CamcorderProfile.get(CamcorderProfile.QUALITY_QVGA));
+				profileNameList.add("QVGA(320*240)");
+			}
+		}
+	}
+
+	//RefreshService关联
+	private MainService mService;
+    private LocalBinder serviceBinder;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className,
+                IBinder service) {
+        		serviceBinder = (LocalBinder)service;
+                mService = serviceBinder.getService();
+                log.setText(mService.getLog());
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+        }
+    };
+
+
 	/*
 	 * 手势识别
 	 */
@@ -537,105 +674,4 @@ public class MainActivity extends Activity implements SurfaceHolder.Callback {
 			overridePendingTransition(R.animator.in_from_left, R.animator.out_to_right);
 		finish();
 	}
-	
-	private void loadSettings() {
-		SharedPreferences settings = getSharedPreferences("setting", MODE_PRIVATE);
-		storageSettingPos = settings.getInt("storage_pos", 0);
-		qualitySettingPos = settings.getInt("quality_pos", 0);
-		timeSettingPos = settings.getInt("time_pos", 0);
-		storageSetting = settings.getInt("storage", getResources().getIntArray(R.array.storage)[0]);
-		timeSetting = settings.getInt("time", getResources().getIntArray(R.array.time)[0]);
-		widthSetting = settings.getInt("width", supportedVideoSizes.get(0).width);
-		heightSetting = settings.getInt("height", supportedVideoSizes.get(0).height);
-		idSetting = settings.getString("id", DEFAULT_ID);
-	}
-	
-	private void settings() {
-        final View preferenceView = LayoutInflater.from(this).inflate(
-                R.layout.settings_dialog, null);
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setView(preferenceView);
-        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				SharedPreferences settings = getSharedPreferences("setting", MODE_PRIVATE);
-		        Editor mEditor = settings.edit();
-		
-		        Spinner recordQuality = (Spinner) preferenceView.findViewById(R.id.quality_select);
-		        int pos = recordQuality.getSelectedItemPosition();
-		        Size size = supportedVideoSizes.get(pos);
-		        mEditor.putInt("quality_pos", pos);
-		        mEditor.putInt("width", size.width);
-		        mEditor.putInt("height", size.height);
-
-		        Spinner recordStorage = (Spinner) preferenceView.findViewById(R.id.storage_select);
-		        pos = recordStorage.getSelectedItemPosition();
-		        mEditor.putInt("storage_pos", pos);
-		        int[] storage = getResources().getIntArray(R.array.storage);
-		        mEditor.putInt("storage", storage[pos]);
-
-		        Spinner recordTime = (Spinner) preferenceView.findViewById(R.id.time_select);
-		        pos = recordTime.getSelectedItemPosition();
-		        mEditor.putInt("time_pos", pos);
-		        int[] time = getResources().getIntArray(R.array.time);
-		        mEditor.putInt("time", time[pos]);
-		
-		        EditText id = (EditText) preferenceView.findViewById(R.id.id);
-		        String idName = id.getText().toString();
-		        mEditor.putString("id", idName);
-		        
-		        mEditor.commit();
-		        mStorageManager.resetStorage(storage[pos]);
-		
-		        loadSettings();
-				mCamera.stopPreview();
-				Camera.Parameters parameters = mCamera.getParameters();
-		    	parameters.setPreviewSize(widthSetting, heightSetting);
-		    	mCamera.setParameters(parameters);
-		    	mCamera.startPreview();
-			}
-        });
-        loadDialogPreferences(preferenceView);
-        builder.show();
-	}
-	
-	private void loadDialogPreferences(View preferenceView) {
-        loadSettings();
-		List<String> sizeList = new ArrayList<String>();
-		for(Size s : supportedVideoSizes) {
-			sizeList.add(s.width + "x" + s.height);
-		}
-        Spinner recordQuality = (Spinner) preferenceView.findViewById(R.id.quality_select);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sizeList);
-        recordQuality.setAdapter(adapter);
-        recordQuality.setSelection(qualitySettingPos);
-
-        Spinner recordStorage = (Spinner) preferenceView.findViewById(R.id.storage_select);
-        recordStorage.setSelection(storageSettingPos);
-
-        Spinner recordTime = (Spinner) preferenceView.findViewById(R.id.time_select);
-        recordTime.setSelection(timeSettingPos);
-        
-        EditText id = (EditText) preferenceView.findViewById(R.id.id);
-        id.setText(idSetting);
-	}
-
-	//RefreshService关联
-	private MainService mService;
-    private LocalBinder serviceBinder;
-    private ServiceConnection mConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName className,
-                IBinder service) {
-        		serviceBinder = (LocalBinder)service;
-                mService = serviceBinder.getService();
-                log.setText(mService.getLog());
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-        }
-    };
-
-
 }
